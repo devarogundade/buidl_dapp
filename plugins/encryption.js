4b1b5871bee2cfdacd5f7a213c8c59aad1e43b0a
Arogundade Ibrahim <devarogundade@gmail.com>
2022-11-10 16:03:21 +0100

    fix

--- plugins/encryption.js
+++ plugins/encryption.js
@@ -1,15 +1,15 @@
-import Vue from "vue"
-import CryptoJS from "crypto-js"
+// import Vue from "vue"
+// import CryptoJS from "crypto-js"
 
-export default ({}, inject) => {
-    inject('encryption', Vue.observable({
-        encrypt: function(data, key) {
-            const encrypt = CryptoJS.AES.encrypt(data, key)
-            return encrypt.toString()
-        },
-        decrypt: function(data, key) {
-            const bytes = CryptoJS.AES.decrypt(data, key);
-            return bytes.toString(CryptoJS.enc.Utf8);
-        },
-    }))
-}
+// export default ({}, inject) => {
+//     inject('encryption', Vue.observable({
+//         encrypt: function(data, key) {
+//             const encrypt = CryptoJS.AES.encrypt(data, key)
+//             return encrypt.toString()
+//         },
+//         decrypt: function(data, key) {
+//             const bytes = CryptoJS.AES.decrypt(data, key);
+//             return bytes.toString(CryptoJS.enc.Utf8);
+//         },
+//     }))
+// }
