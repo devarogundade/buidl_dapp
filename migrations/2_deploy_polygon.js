e24346a42d093ed9097901ce70833a6434b5f1e0
Arogundade Ibrahim <devarogundade@gmail.com>
2022-11-11 05:46:54 +0100

    redployed

--- migrations/2_deploy_polygon.js
+++ migrations/2_deploy_polygon.js
@@ -5,7 +5,7 @@ const MessageSender = artifacts.require("MessageSender");
 const mumbaiGateway = "0xBF62ef1486468a6bd26Dd669C06db43dEd5B849B";
 const mumbaiGasReceiver = "0xbE406F0189A0B4cf3A05C286473D23791Dd44Cc6";
 const destinationName = "binance";
-const destinationAddress = "0x35Ed7DF504E5138Ca3049697a35BBaB10f31819f";
+const destinationAddress = "0x7bfa9F016D76760D38a583036Ad151073D344688";
 
 /* AXELAR polygon mumbai contract */
 module.exports = async function(deployer) {
